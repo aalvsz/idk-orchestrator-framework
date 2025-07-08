7312e717bef87365ad6c33d1a9dcc89367f56bcf
Ander Alvarez Sanz <aalvarezsanz@ideko.es>
2025-07-08 14:15:34 +0200

    Separación de requirements normales y de git.

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 from setuptools import setup, find_packages
-import os
-import sys
 
-def detect_environment():
-    if 'google.colab' in sys.modules:
-        return 'colab'
-    elif 'ipykernel' in sys.modules and 'JPY_PARENT_PID' in os.environ:
-        return 'jupyter'
-    else:
-        return 'normal'
-
-env = detect_environment()
-print(f"Se ha detectado este entorno: {env}")
-
+# leer requirements.txt
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
-
-extra_dependencies = []
-if env == 'normal':
-    extra_dependencies = [
-        "idkrom @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkROM.git@test-package",
-        "idkdoe @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkdoe.git@test-package",
-        "idkopt @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkopt.git@test-package",
-    ]
 
 setup(
     name="idksim",
@@ -32,6 +12,8 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    install_requires=requirements + extra_dependencies,
+    install_requires=[
+        *requirements
+    ],
     python_requires=">=3.11",
 )
