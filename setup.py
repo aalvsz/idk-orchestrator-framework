9c539b7d148a4f94d074a14c9a49aa6d9a63982f
Ander Alvarez Sanz <aalvarezsanz@ideko.es>
2025-07-08 13:05:54 +0200

    Detección automática de entorno en el setup (para instalar desde Google Colab o Jupyter Notebook sin problema).

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
+import os
+import sys
+
+def is_running_in_colab():
+    return 'google.colab' in sys.modules
 
 # leer requirements.txt
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+
+# Dependencias privadas solo si NO estamos en Colab
+extra_dependencies = []
+if not is_running_in_colab():
+    extra_dependencies = [
+        "idkrom @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkROM.git@test-package",
+        "idkdoe @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkdoe.git@test-package",
+        "idkopt @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkopt.git@test-package",
+    ]
 
 setup(
     name="idksim",
@@ -12,12 +26,6 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    install_requires=[
-        *requirements,
-        #"idkfem @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkFEM.git@test-package",
-        "idkrom @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkROM.git@test-package",
-        "idkdoe @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkdoe.git@test-package",
-        "idkopt @ git+https://kodea.danobatgroup.com/dip/precision/ideko/simulation/idkopt.git@test-package",
-    ],
+    install_requires=requirements + extra_dependencies,
     python_requires=">=3.11",
 )
--- src/main.py
+++ src/main.py
@@ -13,16 +13,7 @@ def __test_idkROM():
     quick_run()
 
 if __name__ == "__main__": # para parelizacion, es necesaria la estructura if __name__ == "__main__":
-
-    #idkSIM.runIdkSIM(r"D:\idk_framework\idksimulation\yml\leastsquares_idkROM.yml") 
-    #idkSIM.runIdkSIM("yml/hyperparameter_optim.yml")
-    #idkSIM.runIdkSIM("yml/optim_idkFEM.yml")
-    #idkSIM.runIdkSIM("yml/minimize_idkfem.yml")
-    #idkSIM.runIdkSIM(r"D:\idk_framework\idksimulation\yml\doe_travesano_idkfem.yml") 
-    #idkSIM.runIdkSIM("yml/train_rom_NN.yml")
     
-    #idkSIM.runIdkSIM("yml/optim_simulink.yml")
-
     idkSIM.runIdkSIM(r"D:\idk_framework\idksimulation\yml\test_functions\minimize_curvefit.yml")
 
     #__test_idkFEM()
